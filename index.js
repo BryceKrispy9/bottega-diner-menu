bf4bfad74fa8ec8e4991503df7ed36e234527400
BryceKrispy9 <brycepearson09@gmail.com>
2021-08-22 21:23:20 -0600

    commit on main

---
+++ index.js
@@ -0,0 +1,57 @@
+const mainMenu = [
+    {
+        product: 'Bacon Cheeseburger',
+        price: 13.00
+    },
+    {
+        product: 'Animal Style Cheeseburger',
+        price: 15.00
+    },
+    {
+        product: 'Full Salad',
+        price: 9.50
+    },
+    {
+        product: 'Chicken Wings',
+        price: 11.00
+    },
+    {
+        product: 'Sirloin Steak',
+        price: 21.00
+    },
+    {
+        product: 'Peperroni Pizza',
+        price: 18.50
+    },
+    {
+        product: 'Meatball Sandwich',
+        price: 14.00
+    }
+]
+
+const sideMenu = [
+    {
+        product: 'Onion Rings',
+        price: 5.00
+    },
+    {
+        product: 'French Fries',
+        price: 4.00
+    },
+    {
+        product: 'Side Salad',
+        price: 6.00
+    },
+    {
+        product: 'Sweet Potato Fries',
+        price: 5.00
+    }
+]
+
+const waiterName = ["John", "Michael", "Jessie", "Ryan"];
+
+function displayMenus () {
+    return mainMenu + sideMenu;
+}
+
+console.log(displayMenus());
